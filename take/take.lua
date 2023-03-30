86e5db9be5d064286912d3964fb8bef5c84e4060
Alan Urmancheev <alan.urman@gmail.com>
2023-03-31 01:57:13 +0400

    Take: add tests for different sizes, fix stack overflow

--- take/take.lua
+++ take/take.lua
@@ -4,7 +4,24 @@ local take
 take = function(n, ...)
   local min = math.min(n, select('#', ...))
   if min == 0 then return end
-  return (...), take(n - 1, select(2, ...))
+
+--[[
+  -- Naive implementation, causes stack overflow on large inputs
+  -- Perhaps interpreter could help
+  -- Tail recursion modulo cons?
+  -- en.wikipedia.org/wiki/Tail_call#Tail_recursion_modulo_cons
+  local head = (...)
+  return head, take(n - 1, select(2, ...))
+--]]
+
+----[[
+  local output = {}
+  for i, v in ipairs(table.pack(...)) do
+    output[i] = v
+    if i == n then break end
+  end
+  return table.unpack(output)
+--]]
 end
 
 return take
--- take/test.lua
+++ take/test.lua
@@ -1,14 +1,22 @@
 local take = require'take'
 
-print'Eyeballs test, should be 1, 2, 3, 4, 5 do'
-io.write'  '
-print(take(10, 1, 2, 3, 4, 5))
-print'end'
+-- Generate a table
+local function gen(size)
+  local t = {}
+  for i = 1, size do
+    t[i] = i
+  end
+  return t
+end
 
--- Generate test tables
-local tests = {}
-do
-  local tests_here = {
+local function gen_test(size)
+  local t = gen(size)
+  return {size, t, t}
+end
+
+local function gen_tests(opts)
+  local tests = {}
+  local tests_array = {
     {4, {1, 2, 3, 4}, {1, 2, 3, 4}},
     {10, {1, 2, 3, 4}, {1, 2, 3, 4}},
     {0, {1, 2, 3, 4}, {}},
@@ -16,9 +24,19 @@ do
     {5, {}, {}},
     {0, {}, {}},
   }
-  for i, v in ipairs(tests_here) do
+
+  if opts.sizes then
+    for _, size in ipairs(opts.sizes) do
+      table.insert(tests_array, gen_test(size))
+    end
+  end
+
+  -- Convert array to table
+  for i, v in ipairs(tests_array) do
     tests[i] = {n = v[1], varargs = v[2], result = v[3]}
   end
+
+  return tests
 end
 
 -- Compares two arrays for equality
@@ -28,10 +46,26 @@ local cmp = function(t1, t2)
   return true
 end
 
--- Perform tests
-for _, test in pairs(tests) do
-  io.write'test'
-  local result = table.pack(take(test.n, table.unpack(test.varargs)))
-  assert(cmp(result, test.result))
-  print' = true'
+local function test(opts)
+  print'Eyeballs test, should be 1, 2, 3, 4, 5 do'
+  io.write'  ' print(take(10, 1, 2, 3, 4, 5)) print'end'
+  
+  -- Generate test table
+  local tests = gen_tests(opts)
+  
+  -- Perform tests
+  for _, test in pairs(tests) do
+    io.write'test'
+    local result = table.pack(take(test.n, table.unpack(test.varargs)))
+    assert(cmp(result, test.result))
+    print' = true'
+  end
 end
+
+test{
+  sizes = {
+    1000, -- Naive implementations can handle that
+    10000, -- Naive implementations can’t handle that
+    -- 1000000, -- Table.unpack fails here
+  }
+}
